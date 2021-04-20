578bd0a65c0fbbaae85a9e40ab43f74d94f6af4c
Geetanjali-Arya <garya027@gmail.com>
2021-04-20 12:33:39 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+
+cv = pickle.load(open("model/cv.pkl","rb"))
+clf = pickle.load(open("model/clf.pkl","rb"))
+
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['post'])
+def predict():
+    email = request.form.get('email')
+
+    #predict for spam
+    X_input = cv.transform([email]).toarray()
+    print(X_input.shape)
+    y_pred = clf.predict(X_input)
+    print(y_pred)
+    if y_pred[0] == 0:
+        response = -1
+    else:
+        response = 1
+
+    return render_template('index.html',response=response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
